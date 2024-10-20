a9215ba06c0d2eba16fb1bce9c44e853360cec75
Isadora de Oliveira <isadoradeoliveirapinto@gmail.com>
2024-10-20 10:56:47 -0300

    Add project files.

---
+++ MatchGame/MainWindow.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+6qw
+namespace MatchGame
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            SetUpGame();
+        }
+
+        private void SetUpGame()
+        {
+            List<string> animalEmoji = new List<string>()
+            {
+              "🐈","🐈",
+              "🐩","🐩",
+              "🐘","🐘",
+              "🐢","🐢",
+              "🐬","🐬",
+              "🐠","🐠",
+              "🦀","🦀",
+              "🦢","🦢",
+            };
+
+            Random random = new Random();
+
+            foreach (TextBlock textBlock in mainGrid.Children.OfType<TextBlock>())
+            {
+                int index = random.Next(animalEmoji.Count);
+                string nextEmoji = animalEmoji[index];
+                textBlock.Text = nextEmoji;
+                animalEmoji.RemoveAt(index);
+            }
+        }
+    }
+}
